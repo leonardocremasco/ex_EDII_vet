c407db588d8fc9d8425acb0b2f71b4a4e9093785
Leonardo Cremasco <leoboscariol@gmail.com>
2024-09-14 09:33:54 -0300

    criação das functions

---
+++ ex_aloc_dinamica.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int * initVet ( int *size , int *maxSize ){}
+
+void printVet ( int *v , int size , int maxSize ){}
+
+int * addVet( int *v , int *size , int *maxSize , int e ){}
+
+int find(int *v, int size , int e) {}
+
+int * removeVet( int *v , int *size , int *maxSize , int e ) {}
+
+int main () {}
